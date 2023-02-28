b049e43b1b7eaee7eaaabd8f87fb1fceabda529b
taimoor <taimoortoor@gmail.com>
2023-02-28 04:31:34 -0500

    Task 10 - 14

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -83,4 +83,90 @@ public_users.get('/review/:isbn',function (req, res) {
     res.send(books[ISBN].reviews)
 });
 
+// Get the book list available in the shop (Async)
+
+    async function getBookList() {
+        return books;
+      }
+      public_users.get('/',function (req, res) {
+        getBookList().then(
+          (bk)=>res.send(JSON.stringify(bk, null, 4)),
+          (error) => res.send("denied")
+        );  
+      });
+
+      // Get book list by ISBN (Async)
+
+      async function getFromISBN(isbn) {
+        let book_ = books[isbn];
+        if (book_) {
+          return book_;
+        } else {
+          throw new Error("Unable to find book!");
+        }
+      }
+    
+      public_users.get('/isbn/:isbn', async function (req, res) {
+        const isbn = req.params.isbn;
+        try {
+          const bk = await getFromISBN(isbn);
+          res.send(JSON.stringify(bk, null, 4));
+        } catch (error) {
+          res.send(error);
+        }
+      });
+    
+        // Get book list by author (Async)
+        async function getFromAuthor(author) {
+            let output = [];
+            for (var isbn in books) {
+              let book_ = books[isbn];
+              if (book_.author === author){
+                output.push(book_);
+              }
+            }
+            if (output.length > 0) {
+              return output;
+            } else {
+              throw new Error("No books found for the author!");
+            }
+          }
+          
+          public_users.get('/author/:author',function (req, res) {
+            const author = req.params.author;
+            getFromAuthor(author)
+            .then(
+              result =>res.send(JSON.stringify(result, null, 4))
+            );
+          });
+
+          // Get book list by title
+
+          async function getFromTitle(title) {
+            let output = [];
+            for (var isbn in books) {
+              let book_ = books[isbn];
+              if (book_.title === title){
+                output.push(book_);
+              }
+            }
+            if (output.length > 0) {
+              return output;
+            } else {
+              throw new Error("No books found with the given title!");
+            }
+          }
+
+          public_users.get('/title/:title', async function (req, res) {
+            const title = req.params.title;
+            try {
+              const result = await getFromTitle(title);
+              res.send(JSON.stringify(result, null, 4));
+            } catch (error) {
+              res.send(error);
+            }
+          });
+          
+          
+
 module.exports.general = public_users;
